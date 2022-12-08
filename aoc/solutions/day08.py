702082dbfb5b1aad37b999c500c30975e1a7c967
Jake Scott <scottj1123@gmail.com>
2022-12-08 13:33:55 -0800

    solution for day 8

--- aoc/solutions/day08.py
+++ aoc/solutions/day08.py
@@ -4,20 +4,38 @@ from .shared import Grid, P, Solution
 def main(input_: list[str]) -> Solution:
     grid = Grid.from_lines(input_)
     visible = 0
+    max_score = 0
     for tree in grid.keys():
-        if grid.is_edge(tree) or is_visible(grid, tree):
+        score, vis = visit(grid, tree)
+        if vis:
             visible += 1
+        if score > max_score:
+            max_score = score
 
-    return Solution(visible)
+    return Solution(visible, max_score)
 
 
-def is_visible(g: Grid, p: P) -> bool:
-    h = g.get(p)
+def visit(g: Grid, p: P) -> (int, bool):
+    height = g.get(p)
     row = g.row(p.y)
     col = g.col(p.x)
-    return (
-        all(n < h for n in row[: p.x])
-        or all(n < h for n in row[p.x + 1 :])
-        or all(n < h for n in col[: p.y])
-        or all(n < h for n in col[p.y + 1 :])
-    )
+
+    left_score, left_visible = view(height, row[: p.x], reverse=True)
+    right_score, right_visible = view(height, row[p.x + 1 :])
+    top_score, top_visible = view(height, col[: p.y], reverse=True)
+    bottom_score, bottom_visible = view(height, col[p.y + 1 :])
+
+    visible = left_visible or right_visible or top_visible or bottom_visible
+    score = left_score * right_score * top_score * bottom_score
+    return score, visible
+
+
+def view(height: int, trees: list[int], *, reverse: bool = False) -> (int, bool):
+    max_ = len(trees)
+    if reverse:
+        trees = reversed(trees)
+
+    for idx, h in enumerate(trees, start=1):
+        if h >= height:
+            return idx, False
+    return max_, True
