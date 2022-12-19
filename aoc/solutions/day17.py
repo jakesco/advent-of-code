c4b8f283f808d664b2f81f7ffd9177c45fcaaa80
Jake Scott <jscott@roguecu.org>
2022-12-19 14:27:00 -0800

    attempting cycle detection

--- aoc/solutions/day17.py
+++ aoc/solutions/day17.py
@@ -1,11 +1,15 @@
 from collections import deque
-from dataclasses import dataclass, field
-from itertools import cycle, islice
+from copy import deepcopy
+from dataclasses import dataclass
+from itertools import cycle, islice, zip_longest
 from typing import Iterable, Iterator, NewType
 
 from .shared import Solution
 
+# Test ans: 1514285714288
 FALLEN_ROCKS = 2022
+IMPRESS_THE_ELEPHANTS = 1_000_000_000_000
+EMPTY_ROW = (0, 0, 0, 0, 0, 0, 0)
 
 Row = NewType("Row", tuple[int, ...])
 
@@ -14,10 +18,15 @@ def row_to_str(row: Iterable) -> str:
     return "".join(["#" if r else "." for r in row])
 
 
+def merge_rows(r1: Row, r2: Row) -> Row:
+    return Row(tuple(a | b for a, b in zip(r1, r2)))
+
+
 class Rock:
     def __init__(self, shape: list[deque[int]]):
-        self.shape = shape
-        self.overlap = deque(maxlen=len(shape))
+        self._initial_shape = shape
+        self.shape = deepcopy(shape)
+        self.overlap = deque()
 
     def __str__(self):
         output = []
@@ -25,36 +34,62 @@ class Rock:
             output.append(f"|{row_to_str(row)}|")
         return "\n".join(output)
 
-    def _collide_left(self, stack_rows: list[Row] = None) -> bool:
+    def _reset_shape(self):
+        self.shape = deepcopy(self._initial_shape)
+
+    def _get_overlaps(self) -> Iterator[tuple[Row, Row]]:
+        yield from zip_longest(reversed(self.shape), self.overlap, fillvalue=EMPTY_ROW)
+
+    def _collide_left(self) -> bool:
         if any([r[0] for r in self.shape]):
             return True
+        for a, b in self._get_overlaps():
+            for i in range(1, 7):
+                if a[i] == 1 and b[i - 1] == 1:
+                    return True
         return False
 
-    def _collide_right(self, stack_rows: list[Row] = None) -> bool:
+    def _collide_right(self) -> bool:
         if any([r[-1] for r in self.shape]):
             return True
+        for a, b in self._get_overlaps():
+            for i in range(6):
+                if a[i] == 1 and b[i + 1] == 1:
+                    return True
         return False
 
-    def _collide_down(self, stack_rows: list[Row] = None) -> bool:
-        # TODO
+    def _collide_down(self) -> bool:
+        for a, b in self._get_overlaps():
+            filled = sum(a) + sum(b)
+            if sum(merge_rows(a, b)) < filled:
+                self.overlap.popleft()
+                return True
         return False
 
-    def move(self, direction: int, stack_rows: list[Row] = None) -> bool:
-        if direction < 0 and not self._collide_left(stack_rows):
+    def move(self, direction: int) -> bool:
+        if direction < 0 and not self._collide_left():
             for row in self.shape:
                 row.rotate(-1)
+            # print(f"Moved Left\n{self}", end="\n\n")
             return False
-        if direction > 0 and not self._collide_right(stack_rows):
+        if direction > 0 and not self._collide_right():
             for row in self.shape:
                 row.rotate(1)
+            # print(f"Moved Right\n{self}", end="\n\n")
             return False
-        return self._collide_down(stack_rows)
+        return self._collide_down()
 
     def add_overlap(self, row: Row):
         self.overlap.append(row)
 
     def settle(self) -> list[Row]:
-        return [Row(tuple(islice(r, 7))) for r in reversed(self.shape)]
+        rows = list()
+        for a, b in self._get_overlaps():
+            merged = merge_rows(a, b)
+            rows.append(merged)
+        self.overlap.clear()
+        self._reset_shape()
+        return rows
 
 
 class Chamber(deque):
@@ -62,31 +97,53 @@ class Chamber(deque):
         super(Chamber, self).__init__(*args, **kwargs)
 
     def __str__(self):
-        output = []
+        output = list()
         output.append("|.......|")
         for row in self:
             output.append(f"|{row_to_str(row)}|")
         output.append("+-------+")
         return "\n".join(output)
 
-    def pop_many(self, n: int = 1) -> Iterator[Row]:
+    def pop_many(self, n: int = 1) -> list[Row]:
+        popped = []
         for _ in range(n):
             try:
-                yield self.popleft()
+                popped.append(self.popleft())
             except IndexError:
                 break
+        return popped
+
+
+@dataclass
+class Cycle:
+    height: int
+    rocks: int
 
 
 def main(input_: list[str]) -> Solution:
     chamber = Chamber()
     jets = air_jet(input_[0])
     rocks = falling_rocks()
-    for _ in range(5):
-        simulate_fall(chamber, rocks, jets)
-    print(chamber)
 
-    part1 = part2 = 0
+    rocks_fallen = 0
+    for _ in range(FALLEN_ROCKS):
+        simulate_fall(chamber, rocks, jets)
+        rocks_fallen += 1
     part1 = len(chamber)
+
+    # Try to find a cycle
+    cycle_detected = False
+    while not cycle_detected:
+        cycle_detected = find_cycle(chamber, rocks, jets)
+        rocks_fallen += 1
+        print(rocks_fallen)
+    rock_cycle = Cycle(len(chamber) // 2, rocks_fallen)
+
+    part2 = len(chamber)
+    while rocks_fallen < IMPRESS_THE_ELEPHANTS:
+        part2 += rock_cycle.height
+        rocks_fallen += rock_cycle.rocks
+
     return Solution(part1, part2)
 
 
@@ -94,7 +151,22 @@ def simulate_fall(chamber: Chamber, rocks: Iterator[Rock], jets: Iterator[int]):
     rock = next(rocks)
     for _ in range(4):
         rock.move(next(jets))
+    overlap = 0
+    while overlap < len(chamber):
+        rock.overlap.appendleft(chamber[overlap])
+        if rock.move(0):
+            break
+        rock.move(next(jets))
+        overlap += 1
+    chamber.pop_many(overlap)
     chamber.extendleft(rock.settle())
+
+
+def find_cycle(chamber: Chamber, rocks: Iterator[Rock], jets: Iterator[int]) -> bool:
+    simulate_fall(chamber, rocks, jets)
+    first_half = islice(chamber, len(chamber) // 2)
+    second_half = islice(chamber, len(chamber) // 2, len(chamber))
+    return all([a == b for a, b in zip_longest(first_half, second_half)])
 
 
 def air_jet(pattern: str) -> Iterator[int]:
