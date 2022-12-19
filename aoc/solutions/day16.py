4d9a7ba58c8f14d0d9b346c8f7618ba15400e133
Jake Scott <scottj1123@gmail.com>
2022-12-18 21:43:33 -0800

    finished day16

--- aoc/solutions/day16.py
+++ aoc/solutions/day16.py
@@ -1,18 +1,12 @@
 from __future__ import annotations
 
-from copy import deepcopy
 import math
-import random
 import re
+from collections import deque
 from dataclasses import dataclass
-from functools import partial
-from multiprocessing import Pool, SimpleQueue
+from functools import cached_property
 
-from typing import Iterator
 from .shared import Solution
-
-from itertools import permutations
-
 
 RE_VALVE = re.compile(
     r"Valve (?P<name>[A-Z]{2}).*rate=(?P<rate>\d+);.*valves? (?P<connections>.*)"
@@ -20,7 +14,6 @@ RE_VALVE = re.compile(
 
 START = "AA"
 STEPS = 30
-CHUNK = 100_000
 
 
 @dataclass(frozen=True, eq=True)
@@ -32,8 +25,8 @@ class Node:
 @dataclass
 class Graph:
     nodes: dict[str, Node]
-    edges: dict[str, set[Node]]
-    _adj_matrix: dict[str, dict[str, int]] = None
+    edges: dict[str, list[Node]]
+    adj_matrix: dict[Node, dict[Node, int]] = None
 
     @classmethod
     def from_input(cls, lines: list[str]) -> Graph:
@@ -48,18 +41,18 @@ class Graph:
             connections[name] = connection
         edges = dict()
         for name, conn in connections.items():
-            edges[name] = {nodes[n] for n in conn}
+            edges[name] = [nodes[n] for n in conn]
         graph = cls(nodes, edges)
         graph._build_adj_matrix()
         return graph
 
     def _build_adj_matrix(self):
-        self._adj_matrix = dict()
-        for node in self.nodes.keys():
-            self._adj_matrix[node] = self._shortest_path(node)
+        self.adj_matrix = dict()
+        for node in self.nodes.values():
+            self.adj_matrix[node] = self._shortest_path(node)
 
-    def _shortest_path(self, node: str):
-        distances = {node.name: math.inf for node in self.nodes.values()}
+    def _shortest_path(self, node: Node):
+        distances = {node: math.inf for node in self.nodes.values()}
         distances[node] = 0
 
         q = list(distances.keys())
@@ -67,56 +60,81 @@ class Graph:
         while q:
             q.sort(key=lambda node: distances[node], reverse=True)
             current = q.pop()
-            for adj in self.edges[current]:
-                if adj.name in visited:
+            for adj in self.edges[current.name]:
+                if adj in visited:
                     continue
                 new_dist = distances[current] + 1
-                if new_dist < distances[adj.name]:
-                    distances[adj.name] = new_dist
-                visited.add(adj.name)
+                if new_dist < distances[adj]:
+                    distances[adj] = new_dist
+                visited.add(adj)
         return distances
 
-    def distance(self, n1: str, n2: str) -> int:
-        return self._adj_matrix[n1][n2]
+    def distance(self, n1: Node, n2: Node) -> int:
+        return self.adj_matrix[n1][n2]
 
     def rate(self, node: str) -> int:
         return self.nodes[node].rate
 
-    def important_nodes(self) -> list[str]:
-        return [node.name for node in self.nodes.values() if node.rate > 0]
+    @cached_property
+    def important_nodes(self) -> set[Node]:
+        return {node for node in self.nodes.values() if node.rate > 0}
+
+
+@dataclass
+class Scenario:
+    current: Node | None
+    open_valves: set[Node]
+    steps_left: int
+    steam_released: int = 0
 
 
 def main(input_: list[str]) -> Solution:
     graph = Graph.from_input(input_)
-    part1 = part2 = 0
-    eval_ = partial(eval_scenario, graph=graph)
-    with Pool() as p:
-        results = p.imap_unordered(eval_, permutations(graph.important_nodes(), r=9), chunksize=CHUNK)
-        best = max([result for result in results])
-    print(best)
-
+    part1 = max([s.steam_released for s in enumerate_scenarios(graph)])
+    part2 = best_disjoint_scenarios(enumerate_scenarios(graph, STEPS - 4))
     return Solution(part1, part2)
 
 
-def eval_scenario(scenario: str, *, graph: Graph, max_steps: int = STEPS) -> int:
-    graph = deepcopy(graph)
-    pressure_released = 0
-    steps = 0
-    current = START
-    open_valves = []
-    checked = 0
-    for node in scenario:
-        dist = graph.distance(current, node) + 1
-        steps += dist
-        if steps >= max_steps:
-            steps -= dist
-            break
-        checked += 1
-        pressure_released += sum(open_valves) * dist
-        current = node
-        open_valves.append(graph.rate(current))
+def enumerate_scenarios(graph: Graph, start_steps: int = STEPS) -> list[Scenario]:
+    initial = Scenario(graph.nodes[START], set(), start_steps)
+    q = deque([initial])
+    scenarios = list()
+    while q:
+        s = q.popleft()
 
-    if steps < max_steps:
-        pressure_released += sum(open_valves) * (max_steps - steps)
+        adjacent = adjacent_nodes(graph, s)
+        if len(adjacent) == 0:
+            steam = s.steam_released + sum(
+                [node.rate * s.steps_left for node in s.open_valves]
+            )
+            scenarios.append(Scenario(None, s.open_valves, 0, steam))
+            continue
 
-    return pressure_released
+        for adj, dist in adjacent:
+            steps = s.steps_left - dist
+            steam = s.steam_released + sum([node.rate * dist for node in s.open_valves])
+            open_valves = {adj, *s.open_valves}
+            new_scenario = Scenario(adj, open_valves, steps, steam)
+            q.append(new_scenario)
+    return scenarios
+
+
+def adjacent_nodes(graph: Graph, scenario: Scenario) -> set[(Node, int)]:
+    adjacent = set()
+    for node in graph.important_nodes:
+        if node in scenario.open_valves:
+            continue
+        distance = graph.distance(scenario.current, node) + 1
+        if scenario.steps_left < distance:
+            continue
+        adjacent.add((node, distance))
+    return adjacent
+
+
+def best_disjoint_scenarios(scenarios: list[Scenario]) -> int:
+    scenarios = sorted(scenarios, key=lambda x: x.steam_released, reverse=True)
+    best = scenarios[0]
+    for s in scenarios[1:]:
+        if best.open_valves.isdisjoint(s.open_valves):
+            return best.steam_released + s.steam_released
+    return 0
