96891449071040a2d7d7e9e5843706b51d932760
Jake Scott <scottj1123@gmail.com>
2022-12-22 16:35:23 -0800

    refactored for caching day19

--- aoc/solutions/day19.py
+++ aoc/solutions/day19.py
@@ -4,6 +4,7 @@ import enum
 import math
 import re
 from dataclasses import dataclass
+from functools import cache
 from typing import NewType
 
 from .shared import Solution
@@ -12,91 +13,127 @@ MAX_TICKS = 24
 
 
 class Build(enum.StrEnum):
-    ORE_BOT = "ore_robot"
-    CLAY_BOT = "clay_robot"
-    OBSIDIAN_BOT = "obsidian_robot"
-    GEODE_BOT = "geode_robot"
-    FINISH = ""
+    ORE_BOT = "ore"
+    CLAY_BOT = "clay"
+    OBSIDIAN_BOT = "obsidian"
+    GEODE_BOT = "geode"
+    NOP = ""
 
 
-Sequence = NewType("Sequence", list)
+Sequence = NewType("Sequence", tuple[Build, ...])
 
 
 @dataclass(frozen=True, slots=True)
-class Price:
-    ore: int
-    clay: int
-    obsidian: int
+class Resource:
+    ore: int = 0
+    clay: int = 0
+    obsidian: int = 0
+    geode: int = 0
+
+    @classmethod
+    def make(cls, bot: Build) -> Resource:
+        match bot:
+            case Build.ORE_BOT:
+                return cls(ore=1)
+            case Build.CLAY_BOT:
+                return cls(clay=1)
+            case Build.OBSIDIAN_BOT:
+                return cls(obsidian=1)
+            case Build.GEODE_BOT:
+                return cls(geode=1)
+            case _:
+                return cls()
+
+    def __add__(self, other: Resource) -> Resource:
+        return Resource(
+            self.ore + other.ore,
+            self.clay + other.clay,
+            self.obsidian + other.obsidian,
+            self.geode + other.geode,
+        )
+
+    def __sub__(self, other: Resource) -> Resource:
+        return Resource(
+            self.ore - other.ore,
+            self.clay - other.clay,
+            self.obsidian - other.obsidian,
+            self.geode - other.geode,
+        )
+
+    def __gt__(self, other: Resource) -> bool:
+        return (
+            self.ore > other.ore
+            and self.clay > other.clay
+            and self.obsidian > other.obsidian
+        )
+
+
+@dataclass(frozen=True, slots=True)
+class State:
+    inventory: Resource = Resource()
+    production: Resource = Resource(ore=1)
 
 
 @dataclass(frozen=True, slots=True)
 class Blueprint:
     number: int
-    ore_robot: Price
-    clay_robot: Price
-    obsidian_robot: Price
-    geode_robot: Price
+    ore: Resource
+    clay: Resource
+    obsidian: Resource
+    geode: Resource
 
     @classmethod
     def from_input(cls, blueprint: str) -> Blueprint:
         numbers = map(int, re.findall(r"\d+", blueprint))
         return cls(
             number=next(numbers),
-            ore_robot=Price(next(numbers), 0, 0),
-            clay_robot=Price(next(numbers), 0, 0),
-            obsidian_robot=Price(next(numbers), next(numbers), 0),
-            geode_robot=Price(next(numbers), 0, next(numbers)),
+            ore=Resource(ore=next(numbers)),
+            clay=Resource(ore=next(numbers)),
+            obsidian=Resource(ore=next(numbers), clay=next(numbers)),
+            geode=Resource(ore=next(numbers), obsidian=next(numbers)),
         )
-
-    def get_price(self, action: Build) -> Price:
-        return self.__getattribute__(action)
-
-
-@dataclass(frozen=True, slots=True)
-class State:
-    ticks_left: int = MAX_TICKS
-    ore: int = 0
-    clay: int = 0
-    obsidian: int = 0
-    geode: int = 0
-    ore_robot: int = 1
-    clay_robot: int = 0
-    obsidian_robot: int = 0
-    geode_robot: int = 0
-
-    def stock(self) -> Price:
-        return Price(self.ore, self.clay, self.obsidian)
-
-    def production(self) -> Price:
-        return Price(self.ore_robot, self.clay_robot, self.obsidian_robot)
-
-    def to_dict(self) -> dict[str, int]:
-        return {name: self.__getattribute__(name) for name in self.__slots__}
 
 
 def main(input_: list[str]) -> Solution:
     blueprints = [Blueprint.from_input(line) for line in input_]
-
-    order = Sequence(
-        [
+    sequence = Sequence(
+        (
+            Build.NOP,
+            Build.NOP,
             Build.CLAY_BOT,
+            Build.NOP,
             Build.CLAY_BOT,
+            Build.NOP,
             Build.CLAY_BOT,
+            Build.NOP,
+            Build.NOP,
+            Build.NOP,
             Build.OBSIDIAN_BOT,
             Build.CLAY_BOT,
+            Build.NOP,
+            Build.NOP,
             Build.OBSIDIAN_BOT,
+            Build.NOP,
+            Build.NOP,
             Build.GEODE_BOT,
+            Build.NOP,
+            Build.NOP,
             Build.GEODE_BOT,
-            Build.GEODE_BOT,
-        ]
+            Build.NOP,
+            Build.NOP,
+            Build.NOP,
+        )
     )
-    part1 = eval_sequence(blueprints[0], order, State())
 
-    return Solution(part1.geode)
+    for _ in range(1_000_000):
+        eval_sequence(blueprints[0], sequence, State())
+
+    return Solution()
 
 
+@cache
 def eval_sequence(blueprint: Blueprint, sequence: Sequence, state: State) -> State:
-    print(state)
+    # print(state)
     if len(sequence) <= 0:
         return state
     action = sequence[0]
@@ -105,42 +142,11 @@ def eval_sequence(blueprint: Blueprint, sequence: Sequence, state: State) -> Sta
 
 
 def tick(blueprint: Blueprint, state: State, build: Build) -> State:
-    state_dict = state.to_dict()
-
-    if build == Build.FINISH:
-        state_dict["geode"] += state_dict["ticks_left"] * state_dict["geode_robot"]
-        state_dict["ticks_left"] = 0
-        return State(**state_dict)
-
-    price = blueprint.get_price(build)
-    ticks = required_ticks(price, state.stock(), state.production())
-
-    if ticks > state_dict["ticks_left"]:
-        return tick(blueprint, state, Build.FINISH)
-
     # Gather resources
-    for res in ("ore", "clay", "obsidian", "geode"):
-        state_dict[res] += state_dict[f"{res}_robot"] * ticks
+    inventory = state.inventory + state.production
 
     # Do build
-    for res in ("ore", "clay", "obsidian"):
-        state_dict[res] -= getattr(price, res)
-    state_dict[build] += 1
+    inventory = inventory - getattr(blueprint, build, Resource())
+    production = state.production + Resource.make(build)
 
-    state_dict["ticks_left"] -= ticks
-    return State(**state_dict)
-
-
-def required_ticks(price: Price, inventory: Price, production: Price) -> int:
-    required = set()
-    for res in ("ore", "clay", "obsidian"):
-        current = getattr(inventory, res)
-        target = getattr(price, res)
-        prod = getattr(production, res)
-        if target == 0:
-            continue
-        if target > 0 and prod == 0:
-            return MAX_TICKS
-        r = int(math.ceil((target - current) / prod))
-        required.add(max(r, 0))
-    return max(required) + 1
+    return State(inventory, production)
