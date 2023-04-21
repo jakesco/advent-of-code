f40a2b342b2edb71ff170089c374689a9925188c
Jake Scott <scottj1123@gmail.com>
2023-04-21 15:53:37 -0700

    started day 9

--- aoc/solutions/_2015/day09.py
+++ aoc/solutions/_2015/day09.py
@@ -1,6 +1,25 @@
 from aoc.utils.interfaces import Solution
+from collections import defaultdict
+from dataclasses import dataclass
+from pprint import pprint
+
+import re
+
+RE_CONNECTION = re.compile(r"(.*) to (.*) = (\d+)")
+
+
+@dataclass
+class Connection:
+    node: str
+    dist: int
 
 
 def main(puzzle_input: list[str]) -> Solution:
-    print(puzzle_input)
+    graph: dict[str, list[Connection]] = defaultdict(list)
+    for conn in puzzle_input:
+        m = RE_CONNECTION.match(conn)
+        a, b, dist = m.group(1), m.group(2), int(m.group(3))
+        graph[a].append(Connection(b, dist))
+        graph[b].append(Connection(a, dist))
+    pprint(graph)
     return Solution()
