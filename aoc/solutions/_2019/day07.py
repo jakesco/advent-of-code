60b9f1cd0d8a94b333b1c133e91fc1c6d7cce350
Jake Scott <jake@jakesco.com>
2023-08-26 19:04:34 -0700

    working on 201907 part2

--- aoc/solutions/_2019/day07.py
+++ aoc/solutions/_2019/day07.py
@@ -1,22 +1,63 @@
+from itertools import permutations
+
 from aoc.utils.intcode import IntcodeInterpreter
 from aoc.utils.interfaces import Solution
 
 
 def main(puzzle_input: list[str]) -> Solution:
-    machine = IntcodeInterpreter.loads(puzzle_input[0])
-    sequence = [4, 3, 2, 1, 0]
-    out = 0
-    for s in sequence:
-        out = run_amp(machine, s, out)
-    return Solution(out)
+    part1 = run_part1(puzzle_input[0])
+    part2 = run_part2(puzzle_input[0])
+    return Solution(part1, part2)
 
 
-def run_amp(m: IntcodeInterpreter, phase: int, in0: int) -> int:
-    m.reset()
-    m.input = [phase, in0]
-    m.execute()
-    return m.output.pop(0)
+def run_part1(tape: str) -> int:
+    machine = IntcodeInterpreter.loads(tape)
+    max_ = 0
+    for sequence in permutations(range(5), r=5):
+        out = 0
+        for s in sequence:
+            machine.reset()
+            machine.input = [s, out]
+            machine.execute()
+            out = machine.output.pop(0)
+        max_ = max(max_, out)
+    return max_
+
+
+def run_part2(tape: str) -> int:
+    max_ = 0
+    for sequence in permutations(range(5, 10), r=5):
+        output = 0
+        amp_array = build_amp_array(tape, sequence)
+        output = run_array(amp_array)
+        max_ = max(max_, output)
+    return max_
+
+
+def build_amp_array(tape: str, sequence: tuple[int, ...]) -> list[IntcodeInterpreter]:
+    def fn(tape: str, phase: int) -> IntcodeInterpreter:
+        m = IntcodeInterpreter.loads(tape)
+        m.input = [phase]
+        return m
+
+    return [fn(tape, s) for s in sequence]
+
+
+def run_array(amp_array: list[IntcodeInterpreter]) -> int:
+    output = 0
+    while not all([m._halted for m in amp_array]):
+        for machine in amp_array:
+            machine.input.append(output)
+            machine.execute()
+            output = machine.output.pop(0)
+    return output
 
 
 if __name__ == "__main__":
-    print(main(["3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"]))
+    print(
+        main(
+            [
+                "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+            ]
+        )
+    )
