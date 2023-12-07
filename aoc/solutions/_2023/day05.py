720f7f3a88726e8d62eeaef7d62af62c0f7e0e53
Jake Scott <scottj1123@gmail.com>
2023-12-06 22:56:34 -0800

    2023 07 done

--- aoc/solutions/_2023/day05.py
+++ aoc/solutions/_2023/day05.py
@@ -1,12 +1,14 @@
 from collections.abc import Iterator
 from dataclasses import dataclass
+from itertools import batched
+from math import sqrt
 from typing import Callable, Self
 
 from aoc.utils.functions import apply
 from aoc.utils.interfaces import Solution
 
 # TODO: for part 2 randomly pick some seeds to find a plausible
-# range of local minima. Check all the seeds in that min range.
+#  range of local minima. Check all the seeds in that min range.
 
 
 @dataclass
@@ -42,12 +44,61 @@ def mapper_factory(map_: list[str]) -> Callable[[int], int]:
     return mapper
 
 
+def check_seed_range(
+    start: int, length: int, mappers: list[Callable[[int], int]]
+) -> tuple[int, int]:
+    """Returns an approximate minimum seed value"""
+    step = round(sqrt(length))
+    min_check, min_val = start, apply(start, mappers)
+    check = start + step
+    while check < start + length:
+        val = apply(check, mappers)
+        if val < min_val:
+            min_check, min_val = check, val
+        check += step
+    return min_check, min_val
+
+
+def refine_min_seed(start: int, mappers: list[Callable[[int], int]]) -> int:
+    min_val = apply(start, mappers)
+    left = apply(start - 1, mappers)
+    right = apply(start + 1, mappers)
+    if left < min_val:
+        curr = start - 1
+        val = min_val = left
+        while val <= min_val:
+            curr -= 1
+            val = apply(curr, mappers)
+        return curr + 1
+    if right < min_val:
+        curr = start + 1
+        val = min_val = right
+        while val <= min_val:
+            curr += 1
+            val = apply(curr, mappers)
+        return curr - 1
+    else:
+        return start
+
+
 def main(puzzle_input: list[str]) -> Solution:
     mappers = [mapper_factory(line) for line in feed_input(puzzle_input[2:])]
-    seeds = list(map(int, puzzle_input[0].split(":")[1].split()))
 
+    seeds = list(map(int, puzzle_input[0].split(":")[1].split()))
     part1 = min(apply(seed, mappers) for seed in seeds)
-    return Solution(part1)
+
+    seed_ranges = [
+        (int(a), int(b)) for a, b in batched(puzzle_input[0].split(":")[1].split(), 2)
+    ]
+    approx_min_seed, min_val = check_seed_range(*seed_ranges[0], mappers)
+    for r in seed_ranges[1:]:
+        check, val = check_seed_range(*r, mappers)
+        if val <= min_val:
+            approx_min_seed, min_val = check, val
+    min_seed = refine_min_seed(approx_min_seed, mappers)
+    part2 = apply(min_seed, mappers)
+
+    return Solution(part1, part2)
 
 
 if __name__ == "__main__":
@@ -95,3 +146,4 @@ humidity-to-location map:
     print(s)
     assert s.part1 == 35
     assert s.part2 == 46
+    # 79874952 too high
--- aoc/solutions/_2023/day07.py
+++ aoc/solutions/_2023/day07.py
@@ -1,13 +1,121 @@
+from collections import Counter
+from dataclasses import dataclass
+from enum import IntEnum
+from functools import cache, cmp_to_key, partial
+from typing import Self
+
 from aoc.utils.interfaces import Solution
+
+RANKS = ("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
+RANKS_2 = ("J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A")
+
+
+class HandType(IntEnum):
+    FIVE = 6
+    FOUR = 5
+    FULL = 4
+    THREE = 3
+    TWO = 2
+    ONE = 1
+    HIGH = 0
+
+
+@dataclass(frozen=True)
+class Hand:
+    hand: str
+    bid: int
+
+    @classmethod
+    def from_str(cls, hand: str) -> Self:
+        h, b = hand.split()
+        return cls(h, int(b))
 
 
 def main(puzzle_input: list[str]) -> Solution:
-    return Solution()
+    hands = [Hand.from_str(hand) for hand in puzzle_input]
+    hands1 = sorted(hands, key=cmp_to_key(cmp))
+    cmp2 = partial(cmp, jokers=True, ranks=RANKS_2)
+    hands2 = sorted(hands, key=cmp_to_key(cmp2))
+    return Solution(winnings(hands1), winnings(hands2))
+
+
+@cache
+def hand_type(hand: str, jokers: bool) -> HandType:
+    counts = Counter(hand)
+    if jokers:
+        nj = counts["J"]
+        del counts["J"]
+
+    c = sorted(counts.values(), reverse=True)
+    match c:
+        case [5, *_rest]:
+            return HandType.FIVE
+        case [4, *_rest]:
+            if jokers and nj == 1:
+                return HandType.FIVE
+            return HandType.FOUR
+        case [3, 2, *_rest]:
+            return HandType.FULL
+        case [3, *_rest]:
+            if jokers:
+                if nj == 1:
+                    return HandType.FOUR
+                if nj == 2:
+                    return HandType.FIVE
+            return HandType.THREE
+        case [2, 2, *_rest]:
+            if jokers and nj == 1:
+                return HandType.FULL
+            return HandType.TWO
+        case [2, *_rest]:
+            if jokers:
+                if nj == 3:
+                    return HandType.FIVE
+                if nj == 2:
+                    return HandType.FOUR
+                if nj == 1:
+                    return HandType.THREE
+            return HandType.ONE
+        case [1, *_rest]:
+            if jokers:
+                if nj == 4:
+                    return HandType.FIVE
+                if nj == 3:
+                    return HandType.FOUR
+                if nj == 2:
+                    return HandType.THREE
+                if nj == 1:
+                    return HandType.ONE
+            return HandType.HIGH
+        case _:
+            # Should only get here if JJJJJ
+            return HandType.FIVE
+
+
+def cmp(a: Hand, b: Hand, *, jokers: bool = False, ranks: list[str] = RANKS) -> int:
+    h1, h2 = hand_type(a.hand, jokers), hand_type(b.hand, jokers)
+    if h1 != h2:
+        return -1 if h1 < h2 else 1
+    for c1, c2 in zip(a.hand, b.hand):
+        if c1 == c2:
+            continue
+        x, y = ranks.index(c1), ranks.index(c2)
+        return -1 if x < y else 1
+    return 0
+
+
+def winnings(hands: list[Hand]) -> int:
+    return sum(idx * hand.bid for idx, hand in enumerate(hands, start=1))
 
 
 if __name__ == "__main__":
-    sample = """"""
+    sample = """32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"""
     s = main(sample.split("\n"))
     print(s)
-    # assert s.part1 == 13
-    # assert s.part2 == 30
+    assert s.part1 == 6440
+    assert s.part2 == 5905
+    # 250763402 Too Low
